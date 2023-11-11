d9e7040479ae916f3fc5ec1b6448182c5454ec67
alifanovelia05 <69952596+Alifanoveliasukma@users.noreply.github.com>
2023-11-11 14:18:05 +0700

    test

---
+++ frontend-ecomers-warda/src/api/Api.js
@@ -0,0 +1,17 @@
+//import axios
+
+import axios from 'axios'
+
+
+
+const Api = axios.create({
+
+    //set default endpoint API
+
+    baseURL: 'http://localhost:8000/api'
+
+})
+
+
+
+export default Api
